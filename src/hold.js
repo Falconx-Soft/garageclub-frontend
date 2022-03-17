14cf5e8b503ae133b71c9bdc592b9de1feea3f9c
Tayyab <F2017065009@umt.edu.pk>
2022-03-18 01:26:04 +0500

    pagination

--- src/hold.js
+++ src/hold.js
@@ -3,7 +3,7 @@ import './getItems.css';
 import React from "react"
 import 'react-multi-carousel/lib/styles.css';
 import { Link } from 'react-router-dom';
-
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default function GetItems(props) {
 
@@ -54,18 +54,26 @@ export default function GetItems(props) {
             return new Date(b.created_at) - new Date(a.created_at);
           });
     }
-    const [noOfElement, setnoOfElement] = React.useState(7);
-    const slice = props.temp.slice(0, noOfElement);
-    
-    function loadMoredata(){
-      setnoOfElement(noOfElement+noOfElement);
+    const itemObj =
+    <InfiniteScroll
+    dataLength={7} //This is important field to render the next data
+    next={7}
+    hasMore={true}
+    loader={<h4>Loading...</h4>}
+    endMessage={
+      <p style={{ textAlign: 'center' }}>
+        <b>Yay! You have seen it all</b>
+      </p>
     }
-
-    const itemObj = slice.map((i,index) => {
+    // below props only if you need pull down functionality
+    
+  >
+    {props.temp.map(i => {
         if(i){
             return <Item keyNumber={i.reference} detail={i.model} key={i.id} amount_purchase={i.amount_purchase} amount_sale={i.amount_sale} id={i.id} risk={i.risk} margin={i.margin} setvalidation={props.setvalidation} validation={props.validation} setTemp={props.setTemp} type={i.type} marginTemp={props.marginTemp} created_at={i.created_at} calculation_type={i.calculation_type} cost = {i.costs} tabData={props.tabData} settabData={props.settabData} costList={props.costList} setCostList={props.setCostList} costDetails={props.costDetails} setcostDetails={props.setcostDetails} />
         }
-    })
+    })}
+  </InfiniteScroll>
 
     function selectSort(name){
         props.setSorting(name)
@@ -73,21 +81,21 @@ export default function GetItems(props) {
     
     return (
         <div className='main-div'>
+            {/* <div className="searchDiv">
+                <TextField id="standard-basic" label="Search" variant="standard" onChange={handleChange} />
+            </div> */}
             <div className='sliderDiv'>
                 <div className='filterOption' onClick={()=> selectSort("date")}>By Date</div>
                 <div className='filterOption' onClick={()=> selectSort("margin")}>By Margin</div>
                 <div className='filterOption' onClick={()=> selectSort("price")}>By Selling Price</div>
             </div>
-
-            {itemObj}
-
+              {itemObj}
             <Link to={{
-              pathname: "/add",
-              state:{
-                fromAddComponents: false
-              }
-            }}><div className='addDiv'>+ Create New</div></Link>
-          {noOfElement < props.temp.length ? <button className='loadMoreButton' onClick={loadMoredata}>Load more</button> : <button className='loadMoreButton'>Complete</button> } 
+				pathname: "/add",
+				state:{
+					fromAddComponents: false
+				}
+			}}><div className='addDiv'>+ Create New</div></Link>
         </div>
     )
 }
